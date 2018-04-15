91738fe306d38ce1c356a7456a61e34ff0ea6f4d
johnlindquist <johnlindquist@gmail.com>
2018-04-15 17:22:28 -0600

    🤗

--- src/README.md
+++ src/README.md
@@ -1,3 +1,1 @@
-# Hello World
-
-The simplest React with JSX example:
+# State
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import React from "react"
+import React, { Component } from "react"
 import { render } from "react-dom"
 
-render(<div>Hello world</div>, document.querySelector("#root"))
+class StateDemo extends Component {
+  state = { count: 0 }
+
+  onClick = () => this.setState(state => ({ count: state.count + 1 }))
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.onClick}>+</button>
+      </div>
+    )
+  }
+}
+
+render(<StateDemo />, document.querySelector("#root"))
