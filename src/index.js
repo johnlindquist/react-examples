1ff266b9d58a746dd512ea8777dcf8c3d511bc6c
johnlindquist <johnlindquist@gmail.com>
2018-04-16 15:37:53 -0600

    🤗

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react"
+import React, { Component } from "react"
 import { render } from "react-dom"
 
-const invalid = true
+class Container extends Component {
+  state = { count: 0 }
+  onInc = () => this.setState(state => ({ count: state.count + 1 }))
+  onDec = () => this.setState(state => ({ count: state.count - 1 }))
+
+  render() {
+    return this.props.children({
+      count: this.state.count,
+      onInc: this.onInc,
+      onDec: this.onDec
+    })
+  }
+}
 
 render(
-  <div style={{ textDecoration: invalid ? "line-through" : "underline" }}>
-    Hello
-  </div>,
+  <Container>
+    {({ count, onInc, onDec }) => (
+      <div>
+        <button onClick={onDec}>-</button>
+        <span>{JSON.stringify(count)}</span>
+        <button onClick={onInc}>+</button>
+      </div>
+    )}
+  </Container>,
   document.querySelector("#root")
 )
