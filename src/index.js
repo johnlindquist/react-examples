5fafcf6148fadb4820e7d757ae791d6f998e3da1
johnlindquist <johnlindquist@gmail.com>
2018-04-17 01:01:38 -0600

    🤗

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import { render } from "react-dom"
 
-const URL = `https:/starwars.egghead.training`
+const URL = `https://starwars.egghead.training`
 
 class Container extends Component {
   state = {}
@@ -13,22 +13,22 @@ class Container extends Component {
   }
 
   render() {
-    return this.props.children({
-      person: this.state.person
-    })
+    return this.state.person
+      ? this.props.children({
+          person: this.state.person
+        })
+      : null
   }
 }
 
 render(
   <Container person={0}>
-    {({ person }) =>
-      person ? (
-        <div>
-          <h1>{person.name}</h1>
-          <img src={`${URL}/${person.image}`} alt="" />
-        </div>
-      ) : null
-    }
+    {({ person }) => (
+      <div>
+        <h1>{person.name}</h1>
+        <img src={`${URL}/${person.image}`} alt="" />
+      </div>
+    )}
   </Container>,
   document.querySelector("#root")
 )
