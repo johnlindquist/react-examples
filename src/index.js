c47d4da3ab9c6e6c1a9bf252d203dc3dcc696bdf
johnlindquist <johnlindquist@gmail.com>
2018-04-17 09:43:37 -0600

    🤗

--- src/index.js
+++ src/index.js
@@ -2,24 +2,33 @@ import React, { Component } from "react"
 import { render } from "react-dom"
 
 class Demo extends Component {
+  static Valid = props => (props.isHello ? <span>😊</span> : null)
+  static Invalid = props => (props.isHello ? null : <span>😢</span>)
+  static Input = props => <input type="text" onInput={props.onInput} />
+
+  state = {
+    isHello: false
+  }
+
+  onInput = event => {
+    const { value } = event.target
+    this.setState(state => ({
+      isHello: value === "hello"
+    }))
+  }
+
   render() {
-    return React.Children.map(this.props.children, child => (
-      <div>
-        {React.cloneElement(child, {
-          type: "text",
-          placeholder: "type something here"
-        })}
-        {React.cloneElement(child, { type: "button", value: "Click me" })}
-        {React.cloneElement(child, { type: "number" })}
-        {React.cloneElement(child, { type: "password", value: "password" })}
-      </div>
-    ))
+    return React.Children.map(this.props.children, child =>
+      React.cloneElement(child, { ...this.state, onInput: this.onInput })
+    )
   }
 }
 
 render(
   <Demo>
-    <input />
+    <Demo.Valid />
+    <Demo.Input />
+    <Demo.Invalid />
   </Demo>,
   document.querySelector("#root")
 )
