130d4fe21efc7b7526a9802694cd38c0e622f3d0
johnlindquist <johnlindquist@gmail.com>
2018-04-18 11:19:13 -0600

    🤗

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from "react"
 import { render } from "react-dom"
 import { Form, Text } from "react-form"
 
+const validate = value => ({
+  error:
+    !value || !/.*egghead.*/.test(value) ? "Type 'egghead' somewhere" : null
+})
+
 render(
-  <Form>
+  <Form validateOnMount>
     {formApi => (
-      <form onSubmit={formApi.submitForm}>
-        <Text field="hello" id="hello" />
-        <button type="submit">Submit</button>
-        <code>{JSON.stringify(formApi, null, " ")}</code>
+      <form>
+        <Text field="egghead" id="egghead" validate={validate} />
+        {formApi.errors ? (
+          formApi.errors.egghead ? (
+            <h2>{formApi.errors.egghead}</h2>
+          ) : null
+        ) : null}
       </form>
     )}
   </Form>,
