a760ca665f7ca1a9a5f5a8c57fa4642802431015
johnlindquist <johnlindquist@gmail.com>
2018-04-19 21:25:36 -0600

    🤗

--- src/README.md
+++ src/README.md
@@ -1,1 +1,5 @@
 # Conditional Rendering
+
+Props can determine what should be rendered.
+Ternary conditionals are often used to decide what
+to render.
--- src/index.js
+++ src/index.js
@@ -1,46 +1,9 @@
-import React, { Component } from "react"
+import React from "react"
 import { render } from "react-dom"
 
-const URL = `https://starwars.egghead.training`
+const On = () => <div>On</div>
+const Off = () => <div>Off</div>
 
-class Demo extends Component {
-  state = {
-    loading: true,
-    id: 0,
-    person: null
-  }
+const App = props => (props.on ? <On /> : <Off />)
 
-  loadNext = async () => {
-    this.setState(state => ({ loading: true, id: state.id + 1 }))
-    const person = await fetch(
-      `${URL}/people/${this.state.id}?delay=3000` //add a 3 second delay
-    ).then(res => res.json())
-    this.setState(state => ({ loading: false, person }))
-  }
-
-  componentDidMount() {
-    this.loadNext()
-  }
-
-  render() {
-    return (
-      <div>
-        <button disabled={this.state.loading} onClick={this.loadNext}>
-          Load Next
-        </button>
-        <div>
-          {this.state.loading ? (
-            <img
-              src="https://loading.io/spinners/balls/index.circle-slack-loading-icon.svg"
-              alt=""
-            />
-          ) : (
-            <img src={`${URL}/${this.state.person.image}`} alt="" />
-          )}
-        </div>
-      </div>
-    )
-  }
-}
-
-render(<Demo />, document.querySelector("#root"))
+render(<App on />, document.querySelector("#root"))
