d56dd2c126fd953239076a76473edb43980d638f
johnlindquist <johnlindquist@gmail.com>
2018-04-20 12:30:49 -0600

    🤗

--- src/README.md
+++ src/README.md
@@ -1,10 +1,5 @@
-# `getSnapshotBeforeUpdate`
+# Closest Provider Wins
 
-This method is _rarely_ used. The only scenario:
-
-1.  `setState` updates elements
-2.  The difference between the previous elements and updated elements impacts
-    your component
-3.  You need to check element refs before render and compare them to after the render.
-
-`getSnapshotBeforeUpdate` is _always_ used with `componentDidUpdate`. It gives you the opportunity to catch element differences and pass them to `componentDidUpdate`.
+The `Consumer` will receive its values
+from the closest `Provider` in the component
+hierarchy.
--- src/index.js
+++ src/index.js
@@ -1,72 +1,24 @@
-import React, { Component, Fragment } from "react"
+import React, { Component } from "react"
 import { render } from "react-dom"
 
-class Wrapper extends Component {
-  ref = React.createRef()
-  componentDidMount() {
-    this.ref.current.scrollTop = this.ref.current.scrollHeight
-  }
+const { Consumer, Provider } = React.createContext()
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    const wrapper = this.ref.current
-    return wrapper.scrollTop + wrapper.offsetHeight >= wrapper.scrollHeight
-  }
+const DisplayConsumer = () => (
+  <Consumer>
+    {context => <div>{context}</div>}
+  </Consumer>
+)
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (snapshot) {
-      this.ref.current.scrollTop = this.ref.current.scrollHeight
-    }
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          height: 200,
-          width: 200,
-          overflowY: "scroll",
-          border: "5px solid gray"
-        }}
-        ref={this.ref}
-      >
-        {this.props.children}
+render(
+  <Provider value={"root"}>
+    <DisplayConsumer />
+    <div>
+      <div style={{ margin: "2rem" }}>
+        <Provider value={"nested"}>
+          <DisplayConsumer />
+        </Provider>
       </div>
-    )
-  }
-}
-
-const makeMessage = () =>
-  String.fromCodePoint("0x1f6" + Math.floor(Math.random() * 50).toString(16))
-
-class App extends Component {
-  state = { messages: ["hello?", "hi!", makeMessage()] }
-
-  add = () => {
-    this.setState(state => ({
-      messages: [...state.messages, makeMessage()]
-    }))
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <Wrapper>
-          {this.state.messages.map((message, i) => (
-            <div
-              key={i}
-              style={{
-                outline: "2px solid blue",
-                backgroundColor: i % 2 ? "#eeeeee" : "white"
-              }}
-            >
-              {message}
-            </div>
-          ))}
-        </Wrapper>
-        <button onClick={this.add}>+</button>
-      </Fragment>
-    )
-  }
-}
-
-render(<App />, document.querySelector("#root"))
+    </div>
+  </Provider>,
+  document.querySelector("#root")
+)
