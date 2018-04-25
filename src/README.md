59afda009a27a314e6a170e97805a3f97c6c0f67
johnlindquist <johnlindquist@gmail.com>
2018-04-26 00:56:09 +0200

    🤗

--- src/README.md
+++ src/README.md
@@ -1,3 +1,1 @@
-# Hello World
-
-The simplest React with JSX example:
+# Conditional Styles by Props
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import React from "react"
 import { render } from "react-dom"
+import styled from "styled-components"
 
-render(<div>Hello world</div>, document.querySelector("#root"))
+const Header = styled.h1`
+  border: ${props =>
+    props.primary ? "3px dashed blue" : "initial"};
+
+  color: ${props =>
+    props.primary
+      ? "green"
+      : props.secondary
+        ? "purple"
+        : "initial"};
+`
+
+render(
+  <div>
+    <Header>A normal h1</Header>
+    <Header primary>A primary h1</Header>
+    <Header secondary>A secondary h1</Header>
+  </div>,
+  document.querySelector("#root")
+)
