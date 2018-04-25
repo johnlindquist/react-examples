bfbba290e3b28adde0fc03bccaed7354b81b6cd8
johnlindquist <johnlindquist@gmail.com>
2018-04-25 22:15:20 +0200

    🤗

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { render } from "react-dom"
 
-const Hello = () => <h1>Hello</h1>
+const Hello = props => props.children()
 
 render(
-  <div>{Hello()}</div>,
+  <div>
+    <Hello>{() => <h1>Hi everyone</h1>}</Hello>
+    <Hello>{() => <h1>Hello</h1>}</Hello>
+  </div>,
   document.querySelector("#root")
 )
