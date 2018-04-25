435ac509adbaf489ca69575b2e6edf9af75c1aa5
johnlindquist <johnlindquist@gmail.com>
2018-04-26 00:43:55 +0200

    🤗

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import React from "react"
 import { render } from "react-dom"
+import styled from "styled-components"
 
-render(<div>Hello world</div>, document.querySelector("#root"))
+const HelloWorld = ({ className }) => (
+  <h1 className={className}>Hi</h1>
+)
+
+const StyledHelloWorld = styled(HelloWorld)`
+  color: whitesmoke;
+  text-shadow: 1px 1px 1px black;
+`
+
+render(
+  <div>
+    <HelloWorld />
+    <StyledHelloWorld />
+  </div>,
+  document.querySelector("#root")
+)
